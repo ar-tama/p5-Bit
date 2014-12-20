96fca87e78bf548bd3ed74e4d2a357a1332fe038
ar_tama <arata.makoto@gmail.com>
2014-12-20 21:44:19 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# NAME
+
+Bit - Tiny github and bitbucket helper
+
+# SYNOPSIS
+
+    bit [subcommand]
+    Run "bit -h" for more informations.
+
+# DESCRIPTION
+
+bit is github and bitbucket helper like hub command.
+
+# LICENSE
+
+Copyright (C) ar\_tama.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+ar\_tama <arata.makoto@gmail.com>
---
+++ lib/Bit.pm
@@ -0,0 +1,39 @@
+package Bit;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Bit - It's new $module
+
+=head1 SYNOPSIS
+
+    use Bit;
+
+=head1 DESCRIPTION
+
+Bit is ...
+
+=head1 LICENSE
+
+Copyright (C) ar_tama.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+ar_tama E<lt>makoto.arata@dena.comE<gt>
+
+=cut
+
